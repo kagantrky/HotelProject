3eb689903cb92493338b5721eb59574455de52fb
kagantrky <kaganturkayofficial@gmail.com>
2023-10-12 21:11:29 +0300

    Controllers eklendi, diger duzenlemeler yapıldi admin teması eklendi.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/RoomManager.cs
@@ -0,0 +1,50 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class RoomManager : IRoomService
+    {
+        //Dependency Injection Uygulama
+        //*/
+        //field olusturuldu
+        private readonly IRoomDal _roomDal;
+        //Constructor olusturuldu
+        public RoomManager(IRoomDal roomDal)
+        {
+            _roomDal = roomDal;
+        }
+        // */
+        //RoomService'deki metotlar implemente edildi
+        public void TDelete(Room t)
+        {
+            _roomDal.Delete(t);
+        }
+
+        public Room TGetByID(int id)
+        {
+            return _roomDal.GetByID(id);
+        }
+
+        public List<Room> TGetList()
+        {
+            return _roomDal.GetList();
+        }
+
+        public void TInsert(Room t)
+        {
+            _roomDal.Insert(t);
+        }
+
+        public void TUpdate(Room t)
+        {
+            _roomDal.Update(t);
+        }
+    }
+}
